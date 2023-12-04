cee8c7403eb8b3b7305282ab502c59296ed45091
asdahauoids <151476147+asdahauoids@users.noreply.github.com>
2023-12-03 21:33:29 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,27 +1,27 @@
-import { store } from '../main.js';
-import { embed } from '../util.js';
-import { score } from '../score.js';
-import { fetchEditors, fetchList } from '../content.js';
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../content.js";
 
-import Spinner from '../components/Spinner.js';
-import LevelAuthors from '../components/List/LevelAuthors.js';
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
 
 const roleIconMap = {
-	owner: 'crown',
-	admin: 'user-gear',
-	helper: 'user-shield',
-	dev: 'code',
-	trial: 'user-lock',
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
 };
 
 export default {
-	components: { Spinner, LevelAuthors },
-	template: `
-        <main v-if="loading" class="surface">
+    components: { Spinner, LevelAuthors },
+    template: `
+        <main v-if="loading">
             <Spinner></Spinner>
         </main>
         <main v-else class="page-list">
-            <div class="list-container surface">
+            <div class="list-container">
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
@@ -36,15 +36,10 @@ export default {
                     </tr>
                 </table>
             </div>
-            <div class="level-container surface">
+            <div class="level-container">
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
-                    <div v-if="level.showcase" class="tabs">
-                        <button class="tab type-label-lg selected" :class="{selected: toggledShowcase}" @click="toggledShowcase = true">
-                            <span class="type-label-lg">Showcase</span>
-                        </button>
-                    </div>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
@@ -54,6 +49,10 @@ export default {
                         <li>
                             <div class="type-title-sm">ID</div>
                             <p>{{ level.id }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">Password</div>
+                            <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
@@ -66,10 +65,13 @@ export default {
                                 <p>{{ record.percent }}%</p>
                             </td>
                             <td class="user">
-                                <p class="type-label-lg">{{ record.user }}</p>
+                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
                             </td>
                             <td class="mobile">
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
+                            </td>
+                            <td class="hz">
+                                <p>{{ record.hz }}Hz</p>
                             </td>
                         </tr>
                     </table>
@@ -78,83 +80,96 @@ export default {
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
-            <div class="meta-container surface">
+            <div class="meta-container">
                 <div class="meta">
                     <div class="errors" v-show="errors.length > 0">
                         <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${(store.dark || store.shitty) ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
                                 <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
                     </template>
-                    <h3>The Medium Demon List</h3>
+                    <h3>Submission Requirements</h3>
                     <p>
-                    Template by the Shitty List Team (tsl.pages.dev)
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
                     </p>
                     <p>
-                    Submit Records in the Discord Server
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we never accept records for said level
                     </p>
                 </div>
             </div>
         </main>
     `,
-	data: () => ({
-		list: [],
-		editors: [],
-		loading: true,
-		selected: 0,
-		errors: [],
-		roleIconMap,
-		store,
-		toggledShowcase: false,
-	}),
-	computed: {
-		level() {
-			return this.list[this.selected][0];
-		},
-		video() {
-			if (!this.level.showcase) {
-				return embed(this.level.verification);
-			}
+    data: () => ({
+        list: [],
+        editors: [],
+        loading: true,
+        selected: 0,
+        errors: [],
+        roleIconMap,
+        store
+    }),
+    computed: {
+        level() {
+            return this.list[this.selected][0];
+        },
+        video() {
+            if (!this.level.showcase) {
+                return embed(this.level.verification);
+            }
 
-			return embed(
-				this.toggledShowcase ? this.level.showcase : this.level.verification,
-			);
-		},
-	},
-	async mounted() {
-		// Hide loading spinner
-		this.list = await fetchList();
-		this.editors = await fetchEditors();
+            return embed(
+                this.toggledShowcase
+                    ? this.level.showcase
+                    : this.level.verification
+            );
+        },
+    },
+    async mounted() {
+        // Hide loading spinner
+        this.list = await fetchList();
+        this.editors = await fetchEditors();
 
-		// Error handling
-		if (!this.list) {
-			this.errors = [
-				'Failed to load list. Retry in a few minutes or notify list staff.',
-			];
-		} else {
-			this.errors.push(
-				...this.list
-					.filter(([_, err]) => err)
-					.map(([_, err]) => {
-						return `Failed to load level. (${err}.json)`;
-					}),
-			);
-			if (!this.editors) {
-				this.errors.push('Failed to load list editors.');
-			}
-		}
+        // Error handling
+        if (!this.list) {
+            this.errors = [
+                "Failed to load list. Retry in a few minutes or notify list staff.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => {
+                        return `Failed to load level. (${err}.json)`;
+                    })
+            );
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
+        }
 
-		this.loading = false;
-	},
-	methods: {
-		embed,
-		score,
-	},
+        this.loading = false;
+    },
+    methods: {
+        embed,
+        score,
+    },
 };
